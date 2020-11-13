2e18251f67b8f648fcb4fef61ac67d6aeb907319
Fixer1 <figat.wcale.nie@gmail.com>
2020-11-13 13:39:37 +0000

    test1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -7,13 +7,13 @@ unsigned int transportujFlota(unsigned int towar)
   int towar_p =0;
   int l_zaglowcow=0;
   if(towar!=0){
-  do{
+  while(towar<=towar_p){
   Stocznia stocznia{};
   Statek* s1 = stocznia();
   towar_p += s1->transportuj(); 
   std::cout<<towar_p<<" "<<towar; 
   delete s1;
   if( dynamic_cast<Zaglowiec*>(s1) ){}
-  l_zaglowcow ++;}while(towar<=towar_p);
+  l_zaglowcow ++;}
   std::cout<<"Towar przewieziony. "<<towar<< "Użyto "<<l_zaglowcow<<" żaglowców" ;
 } else{std::cout<<"Brak towaru";}}
