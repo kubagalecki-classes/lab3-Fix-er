76bb7e5b68131605c78ecfb7414ea4c9f32d83fb
Fixer1 <figat.wcale.nie@gmail.com>
2020-11-13 17:09:45 +0000

    solve

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -12,9 +12,7 @@ unsigned int transportujFlota(unsigned int towar)
   Statek* s1 = stocznia();
   int a=s1->transportuj(); 
   towar_p+=a;  
-  Zaglowiec* t = dynamic_cast<Zaglowiec*>(s1);
-  if (t != NULL) {
- // if(typeid(*s1) == typeid(Zaglowiec)){    
+  if(typeid(*s1) == typeid(Zaglowiec)){    
   l_zaglowcow++;}
   delete s1;}
   return l_zaglowcow;
