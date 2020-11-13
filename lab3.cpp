c76611f29fd788ea7137c6c65420249a3b6a2e6b
Fixer1 <figat.wcale.nie@gmail.com>
2020-11-13 12:18:37 +0000

    pierwsze

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+class Figura
+{
+private:
+    double pole;
+
+public:
+    void   setPole(double Pole) { pole = Pole; }
+    double getPole() { return pole; }
+
+    Figura(double p) : pole{p} {}
+};
+class Kolo : public Figura
+{
+public:
+    Kolo(double r) : Figura{3 * r ^ 2} { return getPole(); }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Kolo k{2};
 }
